0171a13e714636d62fb0cf2702cdd202866f2b45
Marisa Ramon <mer0522@gmail.com>
2019-06-10 19:09:48 -0500

    finish most of logic parts of JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -48,10 +48,83 @@ console.log(numBlanks);
 console.log(blanksAndSuccesses);
 }
 
+function checkLetters(letter) {
+    // check if letter exists in code at all
+    var isLetterInWord = false;
+    
+    for (var i = 0; i < numBlanks; i++) {
+        if(selectedWord[i] == letter) {
+            isLetterInWord = true;
+            // alert('letter found');
+        }
+    }
+
+
+// Check where in the word the letters exists and then populate blanksAndSuccesses array
+    if (isLetterInWord) {
+        for (var i = 0; i < numBlanks; i++) {
+            if(selectedWord[i] == letter) {
+                blanksAndSuccesses[i] = letter;
+        }
+   
+     }
+} // letter wasn't found
+    else {
+        wrongLetters.push(letter);
+        numGuesses--;
+    }
+    // Testing / debugging
+    console.log(blanksAndSuccesses);
+
+}
+
+function roundComplete () {
+    console.log("Win count: " + winCount);
+    console.log("Loss count: " + lossCount);
+    console.log("Guesses left: " + guessesLeft);
+
+
+    // Check if user won
+    if(lettersInWord.toString() == blanksAndSuccesses.toString()) {
+        winCount++;
+        alert("You won!");
+        // update the win counter in the html ** put before startGame function in order to see that user has won
+        document.getElementById("winCounter").innerHTML = winCount;
+
+        startGame();
+    }
+    // Check if user lost
+    else if (guessesLeft == 0) {
+        lossCounter++;
+        alert("You lost!");
+
+        // Update the hTML
+        document.getElementById("lossCounter").innerHTML = lossCount;
+
+        startGame();
+    }
+
+}
 
 
 
 // MAIN PROCESS
 // ========================================================
 
+
+// Initiates the code the first time
 startGame();
+
+// Documents key clicks
+document.onkeyup = function (event) {
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(letterGuessed);
+    roundComplete();
+
+
+
+    // Testing / debuggging
+    console.log(letterGuessed);
+
+
+}
